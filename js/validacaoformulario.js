ffdf970d76749fac1442bf33d2fda9a513b46e32
IsraExe <Israel-w@hotmail.com>
2022-03-16 04:46:07 -0300

    validação de form e avatar maker

---
+++ js/validacaoformulario.js
@@ -0,0 +1,68 @@
+let form = document.querySelector('#form-contato')
+let botaoEnviar =  document.querySelector('#btn-enviar')
+let spanMsgEnviada = document.querySelector('.span-msg-enviada')
+let formCorreto = true
+
+botaoEnviar.addEventListener('click', () => {
+    event.preventDefault()
+    let todosOsCampos = document.querySelectorAll('.pega-input-js') 
+    formCorreto = true
+
+    for (i = 0; i < todosOsCampos.length; i++) {
+        let proximoIrmaoDom = todosOsCampos[i].nextElementSibling
+
+        if (todosOsCampos[i].value == '') {
+            proximoIrmaoDom.style.display = 'block'
+            todosOsCampos[i].setAttribute('style', 'border-color: red')
+            spanMsgEnviada.style.display = 'none'
+            proximoIrmaoDom.innerHTML = 'Preencha este campo!'  
+            formCorreto = false              
+        } 
+
+        if (todosOsCampos[i].value != '') {
+            todosOsCampos[i].setAttribute('style', 'border-color: black')
+            proximoIrmaoDom.style.display = 'none'
+
+            validarCampos(todosOsCampos[i], proximoIrmaoDom)
+        }      
+    }
+
+    if (formCorreto) {
+        spanMsgEnviada.style.display = 'block'
+        form.reset()
+    }
+
+})
+
+function validarCampos(todosOsCampos, proximoIrmaoDom) {
+    if (todosOsCampos.value.length > 50 && todosOsCampos.value != '' && todosOsCampos.id == 'nome'
+    || todosOsCampos.value.length > 50 && todosOsCampos.value != '' && todosOsCampos.id == 'assunto'
+    || todosOsCampos.value.length > 300 && todosOsCampos.value != '' && todosOsCampos.id == 'mensagem'
+    || todosOsCampos.value.length < 15 && todosOsCampos.value != '' && todosOsCampos.id == 'mensagem'
+    ) {
+        proximoIrmaoDom.innerHTML = 'Este campo só pode ter até 50 caracteres!'
+        if (todosOsCampos.id == 'mensagem') {
+            proximoIrmaoDom.innerHTML = 'Este campo só pode ter até 300 caracteres!'
+            if (todosOsCampos.value.length < 15) {
+                proximoIrmaoDom.innerHTML = 'Escreva no mínimo 15 caracteres!'
+            } 
+        }  
+        campoComErroCaracteristicas(todosOsCampos, proximoIrmaoDom)
+    } 
+
+    if (todosOsCampos.id == 'email') {
+        let stringBaseEmail = /\S+@\S+\.\S+/
+        if (stringBaseEmail.test(todosOsCampos.value) == false && todosOsCampos.value != '' && todosOsCampos.id == 'email') {
+            proximoIrmaoDom.innerHTML = 'Insira um email válido!'
+            campoComErroCaracteristicas(todosOsCampos, proximoIrmaoDom)
+        } 
+    }
+}
+
+function campoComErroCaracteristicas(todosOsCampos, proximoIrmaoDom) {
+    proximoIrmaoDom.style.display = 'block'
+    todosOsCampos.setAttribute('style', 'border-color: red')
+    spanMsgEnviada.style.display = 'none'
+    return formCorreto = false
+}
+
